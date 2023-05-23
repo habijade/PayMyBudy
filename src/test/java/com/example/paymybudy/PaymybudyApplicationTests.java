6da18f37befa4d94285736b55cff84f98d6a851c
habijade <habijade16@gmail.com>
2023-05-23 16:25:47 +0200

    initial project

---
+++ src/test/java/com/example/paymybudy/PaymybudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paymybudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymybudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
